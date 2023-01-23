19c9b4e9a24bd3be2ae70ad7d2a6c57a99336c82
Udbhav Dalal <udbhavdalal062001@gmail.com>
2023-01-23 10:35:57 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const https = require('https');
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/signup.html");
+
+})
+
+app.post("/failure", function(req, res) {
+    res.redirect("/");
+})
+
+app.post("/", function(req, res) {
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const email = req.body.email;
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+
+        ]
+
+    };
+
+    const jsonData = JSON.stringify(data);
+    const url = "https://us10.api.mailchimp.com/3.0/lists/41a167f0ea";
+    const options = {
+        method: "POST",
+        auth: "udbhav:7712bdf73000027e0e6f44269ec5e4c9-us10"
+    };
+
+    const request = https.request(url, options, function(response) {
+
+        if(response.statusCode === 200) {
+            res.sendFile(__dirname + "/success.html");
+        }
+        else {
+            res.sendFile(__dirname + "/failure.html");
+        }
+
+        response.on("data", function(data) {
+            console.log(JSON.parse(data));
+        });
+    })
+
+    request.write(jsonData);
+    request.end();
+
+    // console.log(firstName, lastName, email);
+    // res.send("Thank you for signing up!");
+})
+
+app.listen(3000, function(req, res) {
+    console.log("Server is up on port 3000");
+})
+
+// APIkey
+// 7712bdf73000027e0e6f44269ec5e4c9-us10
+
+//AudienceID
+// 41a167f0ea
